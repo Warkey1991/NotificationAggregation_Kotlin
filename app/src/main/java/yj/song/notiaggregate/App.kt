ea045e281041620f20c5ed627512668f77684d76
Warkey1991 <warkey1991@gmail.com>
2018-11-09 18:35:36 +0800

    截图

--- app/src/main/java/yj/song/notiaggregate/App.kt
+++ app/src/main/java/yj/song/notiaggregate/App.kt
@@ -8,6 +8,7 @@ import kotlin.properties.Delegates
  * Created by YJ.Song on 2018/11/8.
  */
 class App : Application() {
+
     companion object {
         var instance: App by Delegates.notNull()
     }
